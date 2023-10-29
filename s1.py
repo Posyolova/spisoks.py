ee7c3ff5661acd496c691564b18445675e9db40e
Posyolova <143779118+Posyolova@users.noreply.github.com>
2023-10-29 19:06:10 +0700

    first commit

---
+++ s1.py
@@ -0,0 +1,15 @@
+f=open('s2.txt','r',encoding='utf-8')
+
+students=[]
+for student in f:
+        z=student.split(';')
+        id= int (z[0])
+        var= int (z[2])
+        grup=(z[3])
+        name = z [1]
+        students.append({'id':id,'full_name':name,'var':var,'grup':grup})
+for student in students:
+        print(student)
+  
+f.close()
+
